82cd3301ce7c5f69c0d21ed85a61aef072daa2f4
Behruz <behruzplc@gmail.com>
2025-07-17 14:36:37 -0700

    Darul Kitab birinchi commit

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-BOT_TOKEN = "8188759519:AAFXVEqO6sI34ChHWh3TYh1kR4SS1sI-n5Q"
-CHANNEL_ID = -1001234567890  # Audio fayllar turgan kanal ID'si
-ADMINS = [8027031316, 962773518, 7866416523]  # Ruxsatli adminlar ID ro'yxati
-DEV_USERNAME = "@behruzplc"
-ADMIN_USERNAME = "@uygun_oglu"
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
+DEV_USERNAME = os.getenv("DEV_USERNAME")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
