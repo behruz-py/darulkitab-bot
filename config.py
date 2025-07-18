d79a7e56cefa566cd1929a5e1f5b894410bda546
Behruz <behruzplc@gmail.com>
2025-07-18 11:51:25 -0700

    Birinchi commit config.py edited

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
 DEV_USERNAME = os.getenv("DEV_USERNAME")
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+
+channel_id = os.getenv("CHANNEL_ID")
+if channel_id is None:
+    raise ValueError("CHANNEL_ID environment variable is not set.")
+CHANNEL_ID = int(channel_id)
